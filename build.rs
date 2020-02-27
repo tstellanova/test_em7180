8cf22197fe688667233358ba1fcd57777ac8f30e
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-02-26 19:52:04 -0800

    Allow testing with stm32f3xx

--- build.rs
+++ build.rs
@@ -4,15 +4,25 @@ use std::io::Write;
 use std::path::PathBuf;
 
 fn main() {
-    // Put the linker script somewhere the linker can find it
+
+    #[cfg(feature = "stm32h7x")]
+    let memfile_bytes = include_bytes!("stm32h743_memory.x");
+
+    #[cfg(feature = "stm32f4x")]
+    let memfile_bytes = include_bytes!("stm32f401_memory.x");
+
+    #[cfg(feature = "stm32f3x")]
+    let memfile_bytes = include_bytes!("stm32f334_memory.x");
+
+        // Put the linker script somewhere the linker can find it
     let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
     File::create(out.join("memory.x"))
         .unwrap()
-        .write_all(include_bytes!("memory.x"))
+        .write_all(memfile_bytes) //include_bytes!("memory.x"))
         .unwrap();
     println!("cargo:rustc-link-search={}", out.display());
 
-    // Only re-run the build script when memory.x is changed,
-    // instead of when any part of the source code changes.
-    println!("cargo:rerun-if-changed=memory.x");
+    // // Only re-run the build script when memory.x is changed,
+    // // instead of when any part of the source code changes.
+    // println!("cargo:rerun-if-changed=memory.x");
 }
--- src/main.rs
+++ src/main.rs
@@ -18,56 +18,187 @@ use cortex_m_log::{d_println};
 // use cortex_m::asm;
 use cortex_m_rt::{entry, ExceptionFrame};
 
+#[cfg(feature = "stm32h7x")]
+use stm32h7xx_hal as p_hal;
+
+#[cfg(feature = "stm32f4x")]
 use stm32f4xx_hal as p_hal;
 
+#[cfg(feature = "stm32f3x")]
+use stm32f3xx_hal as p_hal;
+
 use p_hal::prelude::*;
-use p_hal::stm32 as stm32;
-use stm32::I2C1;
-use p_hal::gpio::GpioExt;
+use p_hal::stm32;
+// use stm32::I2C1;
+// use p_hal::gpio::GpioExt;
 // use p_hal::rcc::RccExt;
 
 use em7180::USFS;
+use cortex_m::asm::bkpt;
+// use embedded_graphics::{image::Image1BPP, prelude::*};
+// use ssd1306::{prelude::*, Builder as SSD1306Builder};
 
 #[macro_use]
 extern crate cortex_m_rt;
 
 
 #[cfg(debug_assertions)]
-//type DebugLog = cortex_m_log::printer::dummy::Dummy;
+// type DebugLog = cortex_m_log::printer::dummy::Dummy;
 type DebugLog = cortex_m_log::printer::semihosting::Semihosting<cortex_m_log::modes::InterruptFree, cortex_m_semihosting::hio::HStdout>;
-
-
-// type GpioTypeUserLed1 =  p_hal::gpio::gpioc::PC13<p_hal::gpio::Output<p_hal::gpio::PushPull>>; //stm32f401CxUx
-
-pub type I2cPortType = p_hal::i2c::I2c<I2C1,
-    (p_hal::gpio::gpiob::PB8<p_hal::gpio::AlternateOD<p_hal::gpio::AF4>>,
-     p_hal::gpio::gpiob::PB9<p_hal::gpio::AlternateOD<p_hal::gpio::AF4>>)
->;
 
 
 // cortex-m-rt is setup to call DefaultHandler for a number of fault conditions
 // we can override this in debug mode for handy debugging
 #[exception]
 fn DefaultHandler(_irqn: i16) {
+    bkpt();
     d_println!(get_debug_log(), "IRQn = {}", _irqn);
 }
 
 // cortex-m-rt calls this for serious faults.  can set a breakpoint to debug
 #[exception]
-fn HardFault(ef: &ExceptionFrame) -> ! {
-    panic!("HardFault: {:?}", ef);
+fn HardFault(_ef: &ExceptionFrame) -> ! {
+    bkpt();
+    loop {}
+    //panic!("HardFault: {:?}", ef);
 }
 
 /// Used in debug builds to provide a logging outlet
 #[cfg(debug_assertions)]
 fn get_debug_log() -> DebugLog {
-    //cortex_m_log::printer::Dummy::new()
+    // cortex_m_log::printer::Dummy::new()
     semihosting::InterruptFree::<_>::stdout().unwrap()
 }
 
-#[entry]
-fn main() -> ! {
-    let mut log = get_debug_log();
+
+
+#[cfg(feature = "stm32f3x")]
+fn run_it() -> ! {
+
+    let dp = stm32::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    // Set up the system clock
+    let mut rcc = dp.RCC.constrain();
+    let mut flash = dp.FLASH.constrain();
+
+    // HSI: use default internal oscillator
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    // let clocks = rcc.cfgr.freeze();
+    // HSE: external crystal oscillator must be connected
+    //let clocks = rcc.cfgr.use_hse(SystemCoreClock.hz()).freeze();
+
+    let mut delay_source =  p_hal::delay::Delay::new(cp.SYST, clocks);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    // let gpioc = dp.GPIOC.split();
+
+    let mut user_led1 = gpiob.pb6.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    user_led1.set_high().unwrap();
+
+
+    // setup i2c1 and imu driver
+    let scl = gpiob.pb8
+        .into_open_drain_output(&mut gpiob.moder, &mut gpiob.otyper)
+        .into_af4(&mut gpiob.moder, &mut gpiob.afrh);
+    //scl.internal_pull_up(&mut gpiob.pupdr, true);
+
+    let sda = gpiob.pb9
+        .into_open_drain_output(&mut gpiob.moder, &mut gpiob.otyper)
+        .into_af4(&mut gpiob.moder, &mut gpiob.afrh);
+    //sda.internal_pull_up(&mut gpiob.pupdr, true);
+
+    let i2c_port = p_hal::i2c::I2c::i2c1(
+        dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
+    //let i2c_bus = shared_bus::CortexMBusManager::new(i2c_port);
+
+    // // setup i2c1 and imu driver
+    // // NOTE: stm32f401CxUx lacks external pull-ups on i2c pins
+    // // NOTE: eg f407 discovery board already has external pull-ups
+    // let scl = gpiob.pb8
+    //     .into_alternate_af4()
+    //     .internal_pull_up(true)
+    //     .set_open_drain();
+    //
+    // let sda = gpiob.pb9
+    //     .into_alternate_af4()
+    //     .internal_pull_up(true)
+    //     .set_open_drain();
+    // let i2c_port = p_hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks);
+
+    let mut ahrs = USFS::new_inv_usfs_03(i2c_port, //i2c_bus.acquire(),
+                                         em7180::EM7180_DEFAULT_ADDRESS,
+                                         0, //unused for now
+                                           false).unwrap();
+
+
+    // let fflags = ahrs.check_feature_flags().unwrap_or(0);
+    // if fflags != 0x05 { //barometer + temperature sensor installed
+    //     d_println!(get_debug_log(), "fflags {}", fflags);
+    // }
+
+    // // Set up the display
+    // let mut disp: GraphicsMode<_> = SSD1306Builder::new().connect_i2c(i2c_bus.acquire()).into();
+    // disp.init().unwrap();
+    // disp.flush().unwrap();
+    //
+    // // Display the rustacean
+    // // let im = Image1BPP::new(include_bytes!("./ssd1306-image.data"), 128, 64);
+    // let im = Image1BPP::new(include_bytes!("./compass_64.raw"), 64, 64);
+    // disp.draw(im.into_iter());
+    // disp.flush().unwrap();
+    //
+    // // Set up state for the loop
+    // let mut orientation = DisplayRotation::Rotate0;
+    // disp.set_rotation(orientation).unwrap();
+
+    //set initial states of user LEDs
+    user_led1.set_low().unwrap();
+
+    delay_source.delay_ms(1u8);
+    //cortex_m::asm::delay(1_000);
+
+    loop {
+        // disp.set_rotation(orientation).unwrap();
+        // disp.flush().unwrap();
+        // orientation = get_next_rotation(orientation);
+
+        // if let Ok(err_check) = ahrs.check_errors() {
+        //     if 0 != err_check {
+        //         d_println!(get_debug_log(), "err {:?}", err_check);
+        //         break;
+        //     }
+        // }
+
+        if ahrs.quat_available() {
+            let qata =  ahrs.read_sentral_quat_qata();
+            // if let Ok(_quat) = qata {
+            //     d_println!(get_debug_log(), ".");
+            //     //d_println!(log, "{:?}", quat);
+            // }
+            if qata.is_err() {
+                bkpt();
+                //panic!("read_err {:?}", qata);
+            }
+            else {
+                user_led1.set_high().unwrap();
+
+               // user_led1.toggle().unwrap();
+            }
+        }
+        //cortex_m::asm::delay(1_000);
+        delay_source.delay_ms(1u8);
+    }
+
+    //panic!("early termination")
+
+
+}
+
+#[cfg(feature = "stm32f4x")]
+fn run_it() -> ! {
 
     let dp = stm32::Peripherals::take().unwrap();
     let cp = cortex_m::Peripherals::take().unwrap();
@@ -75,9 +206,9 @@ fn main() -> ! {
     // Set up the system clock
     let rcc = dp.RCC.constrain();
     // HSI: use default internal oscillator
-    let clocks = rcc.cfgr.freeze();
+    //let clocks = rcc.cfgr.freeze();
     // HSE: external crystal oscillator must be connected
-    //let clocks = rcc.cfgr.use_hse(SystemCoreClock.hz()).freeze();
+    let clocks = rcc.cfgr.use_hse(25_000_000.hz()).freeze();
 
     let mut delay_source =  p_hal::delay::Delay::new(cp.SYST, clocks);
 
@@ -85,8 +216,7 @@ fn main() -> ! {
     let gpioc = dp.GPIOC.split();
 
     let mut user_led1 = gpioc.pc13.into_push_pull_output(); //f401CxUx
-    //set initial states of user LEDs
-    user_led1.set_high().unwrap();
+
 
     // setup i2c1 and imu driver
     // NOTE: stm32f401CxUx lacks external pull-ups on i2c pins
@@ -101,43 +231,88 @@ fn main() -> ! {
         .internal_pull_up(true)
         .set_open_drain();
     let i2c_port = p_hal::i2c::I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks);
+    let i2c_bus = shared_bus::CortexMBusManager::new(i2c_port);
 
-    let mut driver = USFS::new_st_usfs1(i2c_port,
-                                      em7180::EM7180_DEFAULT_ADDRESS,
-                                      0, //unused for now
+    let mut ahrs = USFS::new_inv_usfs_03(i2c_bus.acquire(),
+                                         em7180::EM7180_DEFAULT_ADDRESS,
+                                         0, //unused for now
                                       false).unwrap();
 
 
-    let fflags = driver.check_feature_flags().unwrap_or(0);
+    let fflags = ahrs.check_feature_flags().unwrap_or(0);
     if fflags != 0x05 { //barometer + temperature sensor installed
-        d_println!(log, "fflags {}", fflags);
+        d_println!(get_debug_log(), "fflags {}", fflags);
    }
+
+    // Set up the display
+    let mut disp: GraphicsMode<_> = SSD1306Builder::new().connect_i2c(i2c_bus.acquire()).into();
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    // Display the rustacean
+    let im = Image1BPP::new(include_bytes!("./haxors_128.raw"), 128, 64);
+    disp.draw(im.into_iter());
+    disp.flush().unwrap();
+
+    // Set up state for the loop
+    let mut orientation = DisplayRotation::Rotate0;
+    disp.set_rotation(orientation).unwrap();
+
+
+    //set initial states of user LEDs
+    user_led1.set_high().unwrap();
 
     delay_source.delay_ms(1u8);
 
+
     loop {
-        if let Ok(err_check) = driver.check_errors() {
+        disp.set_rotation(get_next_rotation(orientation)).unwrap();
+        disp.flush().unwrap();
+        orientation = get_next_rotation(orientation);
+
+        if let Ok(err_check) = ahrs.check_errors() {
             if 0 != err_check {
-                d_println!(log, "err {:?}", err_check);
+                d_println!(get_debug_log(), "err {:?}", err_check);
                 break;
             }
         }
-        if driver.quat_available() {
-            //bkpt();
-            let qata =  driver.read_sentral_quat_qata();
-            if let Ok(quat) = qata {
-                d_println!(log, "{:?}", quat);
-            }
-            else {
-                d_println!(log, "{:?}", qata);
-            }
+        if ahrs.quat_available() {
+            user_led1.toggle().unwrap();
+            //d_println!(get_debug_log(), ".");
+            //let qata =  ahrs.read_sentral_quat_qata();
+            //if let Ok(_quat) = qata {
+            //    user_led1.toggle().unwrap();
+                //d_println!(get_debug_log(), ".");
+                //d_println!(get_debug_log(), "{:?}", quat);
+            //}
+            //else {
+            //    bkpt();
+            //    panic!("read_err {:?}", qata);
+            //}
         }
+        //user_led1.toggle().unwrap();
         delay_source.delay_ms(1u8);
-        user_led1.toggle().unwrap();
     }
 
     loop {
-        // we reach here if something went wrong
-        delay_source.delay_ms(250u8);
+        d_println!(get_debug_log(), "early termination");
+        bkpt();
     }
+    //panic!("early termination")
+
+}
+
+// pub fn get_next_rotation(rotation: DisplayRotation) -> DisplayRotation {
+//     return match rotation {
+//         DisplayRotation::Rotate0 => DisplayRotation::Rotate90,
+//         DisplayRotation::Rotate90 => DisplayRotation::Rotate180,
+//         DisplayRotation::Rotate180 => DisplayRotation::Rotate270,
+//         DisplayRotation::Rotate270 => DisplayRotation::Rotate0,
+//     };
+// }
+
+
+#[entry]
+fn main() -> ! {
+    run_it();
 }
